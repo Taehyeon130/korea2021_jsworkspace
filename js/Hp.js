1b31c1efb6e02f90f79c5cdf509d9db2ebb09bf0
taehyeon130 <taehyeongim59@gmail.com>
2021-03-25 20:32:20 +0900

    Ver

---
+++ js/Hp.js
@@ -0,0 +1,22 @@
+class Hp{
+    constructor(container,src,width,height,x,y){
+        this.container = container;
+        this.img=document.createElement("img");
+        this.src=src;
+        this.width=width;
+        this.height=height;
+        this.x=x;
+        this.y=y;
+    
+        this.img.src = this.src;
+ 
+        this.img.style.width = this.width+"px";
+        this.img.style.height = this.height+"px";
+        
+        this.img.style.position = "absolute";
+        this.img.style.left = this.x+"px";
+        this.img.style.top = this.y+"px";
+
+        this.container.appendChild(this.img);
+    }
+}
